67a33d63319cc56019b0456ca59da03f6b71f52b
ElaineCosta <elainerscosta@hotmail.com>
2022-09-13 15:58:41 -0300

    : feat: criando pasta de routes e ajustando o app.ts para receber as routes e middleware de erros

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './errors/errorMiddleware';
+import routes from './routes/routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.use(errorMiddleware);
 
 export default app;
