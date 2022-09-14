b489de22ca3fe042e3fc50874cd4e7ef696acbbf
ElaineCosta <elainerscosta@hotmail.com>
2022-09-14 17:23:46 -0300

    :feat: incluindo rota para login

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/login', route.loginRoute);
 app.use('/products', route.productRoutes);
 app.use('/orders', route.ordersRoutes);
 app.use('/users', route.usersController);
