ad7fa42ab9b255fd3978e2ee80a6f6e67d90a309
ElaineCosta <elainerscosta@hotmail.com>
2022-09-14 20:16:47 -0300

    :feat: incluindo o middleware de validacao do product na rota

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import productController from '../products.msc/controller.product';
+import validateProduct from '../middlewares/validateProduct';
 
 const productRoute: Router = Router();
 
 productRoute.get('/', productController.getAll);
-productRoute.post('/', productController.create);
+productRoute.post('/', validateProduct, productController.create);
 
 export default productRoute;
