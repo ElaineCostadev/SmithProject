f7030e1bc1b70c79b11d937c48685b07f139a29a
ElaineCosta <elainerscosta@hotmail.com>
2022-09-18 20:37:52 -0300

    :feat: criacao da funcao create em ordersController para retornar as informações inseridas(cadastradas)

--- src/orders.msc/controller.order.ts
+++ src/orders.msc/controller.order.ts
@@ -6,6 +6,13 @@ const ordersController = {
     const result = await ordersService.getAll();
     return res.status(200).json(result);
   },
+
+  create: async (req: Request, res: Response) => {
+    const { productsIds } = req.body;
+    const { user } = req.body;
+    const result = await ordersService.create(user.username, productsIds);
+    return res.status(201).json(result);
+  },
 };
 
 export default ordersController;
