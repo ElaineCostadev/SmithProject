57e1fa6ffd12b412dccd2bf41a011f688c6cf401
ElaineCosta <elainerscosta@hotmail.com>
2022-09-18 20:36:25 -0300

    :feat:? incluindo rota post em orders para incluir e atualizar os products

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import ordersController from '../orders.msc/controller.order';
+import validateToken from '../middlewares/validateToken';
+import validateOrders from '../middlewares/validateOrders';
 
 const orderRoute: Router = Router();
 
 orderRoute.get('/', ordersController.getAll);
+orderRoute.post('/', validateToken, validateOrders, ordersController.create);
 
 export default orderRoute;
