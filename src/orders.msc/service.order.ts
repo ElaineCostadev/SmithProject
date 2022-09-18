33553f414cd4264d1dafb76fe0524e88d3c73c14
ElaineCosta <elainerscosta@hotmail.com>
2022-09-18 20:45:35 -0300

    :feat: criando funcao create da camada service em Order para retornar as informaçoes desejadas

--- src/orders.msc/service.order.ts
+++ src/orders.msc/service.order.ts
@@ -1,10 +1,20 @@
+import usersModel from '../users.msc/model.user';
 import ordersModel from './model.order';
-// import Iorders from '../interfaces/orders.interface';
+import CustomError from '../errors/CustomError';
 
 const ordersService = {
   getAll: async () => {
     const result = await ordersModel.getAll();
     return result;
+  },
+
+  create: async (username: string, productsIds: []) => {
+    // entro o id do meu usuario
+    const resultUser = await usersModel.getByPk(username);
+
+    const orderResult = await ordersModel.create(resultUser, productsIds);
+    if (!orderResult) throw new CustomError(400, 'Algo deu de errado, não foi possivel cadastrar');
+    return orderResult;
   },
 };
 
